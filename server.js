083a6b5ccc60e415e0ffea6548140875f9e8b32e
Tanvir <tkdarkshadow0@gmail.com>
2025-11-27 13:12:22 +0600

    message

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// ✅ Allow both localhost and Vercel domain
+// Allow both localhost and Vercel domain
 const allowedOrigins = [
   "http://localhost:3000",
   "https://minimart-alpha.vercel.app",
